c77097b448a2cc2f77adadd634a1cc893455f320
Magda <magdap.265@gmail.com>
2019-09-02 19:27:02 +0200

    dopracowanie input oraz funkcjonalności przycisku; po dodaniu elementu input zostaje wyczyszczony oraz nowe zadanie nie zostanie dodane gdy input jest pusty

--- src/AddNewItem.js
+++ src/AddNewItem.js
@@ -2,23 +2,20 @@ import React from 'react';
 import './AddNewItem.css';
 
 class AddNewItem extends React.Component {
-    constructor(props){
-        super(props)
-            this.state = {
-            inputText: ''
-        }    
-    }
-    
-    updateInputText = (event) => {
-        this.setState({inputText: event.target.value})
-    }
-
     render(){
         return(
             <ul>
                 <li>
-                    <input className = "addInput" type="text" placeholder="New Item... :)" onChange={this.updateInputText}/>
-                    <button className = "addButton" onClick={this.props.addTask}> + </button>
+                    <input className = "addInput" 
+                            type="text" 
+                            onChange={this.props.updateInputText}
+                            value={this.props.inputText}
+                            placeholder={'...add new item :)'}
+                            
+                    />
+                    <button className = "addButton"  
+                            onClick={this.props.addTask}
+                    > + </button>
                 </li>
             </ul>
         )
--- src/List.js
+++ src/List.js
@@ -7,14 +7,24 @@ class List extends React.Component{
     state = {
         tasks: ['Make a dinner',
                 'Go for  walk',
-                'Meet with friends']
+                'Meet with friends'],
+        inputText: ''
     }
 
-    addTask = () =>{
-        const list = this.state.tasks
-        list.push(this.state.inputText)
-        this.setState({tasks: list})
-        console.log()
+    updateInputText = (event) => {
+        this.setState({inputText: event.target.value})
+    }
+
+    addTask = () => {
+        if(this.state.inputText !== ''){
+            const list = this.state.tasks
+            list.push(this.state.inputText)
+            this.setState({
+                tasks: list,
+                inputText: ''
+            })
+        }
+        console.log(this.state.inputText)
     }
 
     render(){
@@ -22,7 +32,11 @@ class List extends React.Component{
             <div>
                 <h2>{this.props.title}</h2>
                 {this.state.tasks.map((task, index) => <Item item = {task} key={index}/>)}
-                <AddNewItem tasks={this.state.tasks} addTask={this.addTask}/>
+                <AddNewItem tasks={this.state.tasks} 
+                            inputText={this.state.inputText} 
+                            addTask={this.addTask} 
+                            updateInputText={this.updateInputText}
+                />
             </div>
         )
     }
